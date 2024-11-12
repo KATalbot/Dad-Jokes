0115ad0f628d7484b93d828a0429effe280e84f3
KATalbot <talbot.keith@gmail.com>
2024-11-12 12:50:32 -0500

    Initial Commit

---
+++ Dad Jokes/Dad_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dad_JokesApp.swift
+//  Dad Jokes
+//
+//  Created by keith Talbot on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Dad_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
